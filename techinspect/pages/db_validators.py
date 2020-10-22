08700b12956941eea795e3f9f8e237609a4a3831
jn <jackmnitti@gmail.com>
2020-10-22 02:15:17 -0500

    Added static folder

---
+++ techinspect/pages/db_validators.py
@@ -0,0 +1,13 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+from datetime import datetime #For validate_car_year
+
+
+def validate_car_year(value):
+    if value > datetime.year + 1:
+        raise ValidationError(
+        _('The year value %(value) cannot be more than %(acceptable_year)'),
+        params={'value': value, 'acceptable_year': datetime.year + 1},
+        )
+
