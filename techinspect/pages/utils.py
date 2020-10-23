a8b16a3644026fc2029c8b6fc411e381de9dc870
jn <jackmnitti@gmail.com>
2020-10-23 00:45:32 -0500

    Fixing files not being added correctly

--- techinspect/pages/utils.py
+++ techinspect/pages/utils.py
@@ -3,6 +3,6 @@ from pages.models import *
 def login(email, pswd):
     if(user := TIUser.objects.get(username=email)):
         if user.check_password(pswd):
-            print("It worked lol")
+            return True
         else:
-            print("Failed")
+            return False
--- techinspect/pages/views.py
+++ techinspect/pages/views.py
@@ -1,8 +1,22 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 from django.shortcuts import render
 
+from pages import forms
+from pages import utils
+
 def login_render(request):
-    return render(request, 'login/index.html')
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            #IMPORTANT: TO ACCESS FORM DATA ALWAYS USE cleaned_data['name of field']
+            if utils.login(form.cleaned_data['username'], form.cleaned_data['password']):
+                print("Getting here!!!")
+                return HttpResponseRedirect('home/') 
+    else:
+        form = forms.LoginForm()
+    return render(request, 'login/index.html', {'form': form})
+
+
 def homepage_render(request):
     return render(request, 'home/index.html')
