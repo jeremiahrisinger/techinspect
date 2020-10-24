b869dabb1d132ae6e36d20421c314c87a1d67177
jn <jackmnitti@gmail.com>
2020-10-24 13:55:22 -0500

    Added a SignupForm class that corresponds to form found in pages/templates/signup/index.html

--- techinspect/pages/forms.py
+++ techinspect/pages/forms.py
@@ -4,3 +4,9 @@ class LoginForm(forms.Form):
     #max_length matches max_length for username field in pages/models.TIUser
     username = forms.CharField(label="username", max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+#TODO convert this to a ModelForm so we can easily save it in utils.add_user
+class SignupForm(forms.Form):
+    username = forms.CharField(label="username", max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    user_image = forms.ImageField()
