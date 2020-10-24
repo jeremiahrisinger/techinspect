b1e4c068e71fed30d82f9b85dd41ded9edfee155
jn <jackmnitti@gmail.com>
2020-10-23 21:33:10 -0500

    Added message to notify a user if there is no corresponding account at login attempt

--- techinspect/pages/forms.py
+++ techinspect/pages/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class LoginForm(forms.Form):
     #max_length matches max_length for username field in pages/models.TIUser
-    username = forms.CharField(label="username", max_length=150)
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(label="username", max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
