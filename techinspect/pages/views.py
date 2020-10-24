1ecbfa48a5e41e254636d0e8daca069a4ca61dc4
jn <jackmnitti@gmail.com>
2020-10-23 21:36:56 -0500

    There is now a message with a hyperlink to the (not yet made) signup page

--- techinspect/pages/views.py
+++ techinspect/pages/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 from django.shortcuts import render
-
+from django.utils.html import mark_safe
 from pages import forms
 from pages import utils
 
@@ -10,10 +11,13 @@ def login_render(request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
             #IMPORTANT: TO ACCESS FORM DATA ALWAYS USE cleaned_data['name of field']
-            if utils.login(form.cleaned_data['username'], form.cleaned_data['password']):
-                return HttpResponseRedirect('home/') 
-            else:
-                messages.error(request, "Login failed due to incorrect username/password")
+            try:
+                if utils.login(form.cleaned_data['username'], form.cleaned_data['password']):
+                    return HttpResponseRedirect('home/') 
+                else:
+                    messages.error(request, "Login failed due to incorrect username/password")
+            except ObjectDoesNotExist:
+                    messages.error(request, mark_safe("Your username isn't associated with an account. </br>Click <a href='/signup'>signup</a> to create an account."))
     else:
         form = forms.LoginForm()
     return render(request, 'login/index.html', {'form': form})
